12601b31e99794d86cf290abe7ae9698b34737f6
Andrea Gallina <4everamd@gmail.com>
2011-11-09 19:12:22 +0100

    aggiunta SOLO la gianceza reale

--- mrp.py
+++ mrp.py
@@ -5,12 +5,32 @@ import pooler, tools
 from osv import fields, osv
 
 class mrp_production(osv.osv):
+    
+
+    def qty_available(self, cr, uid, ids,data, arg, context=None):
+        var={}
+        obj=self.pool.get('mrp.production')
+        for riga in obj.browse(cr, uid, ids):
+            qty = riga.product_id.qty_available
+            var[riga.id] = {'qty_available': qty}
+        return var    
+   
+# SE CALCOLA ANCHE LA GIACENZA VIRTUALE PERDE IL CALCOLO DELLA GIACENZA VIRTUALE
+#    def virtual_available(self, cr, uid, ids, data, arg, context=None): 
+#       var = {} 
+#       obj=self.pool.get('mrp.production')
+#       for riga in obj.browse(cr, uid, ids):
+#              virtual = riga.product_id.virtual_available
+#              var[riga.id]= {'virtual_available': virtual}
+#       return var
+
+
+    
     _inherit = 'mrp.production' 
     #Do not touch _name it must be same as _inherit
     #_name = 'mrp.production' cr
     _columns ={
-               'qty_available':fields.one2many('product.product', 'product_tmpl_id', 'Giacenza Reale'),
-                'virtual_available':fields.one2many('product.product', 'product_tmpl_id', 'Giacenza Virtuale'),
-
+               'qty_available' : fields.function(qty_available, method=True, string='Giacenza Reale', multi='sums'),
+               #'virtual_available':fields.function(virtual_available, method=True, string='Giacenza Virtuale', multi='sums'),
                }
 mrp_production()
